d316058817a80e522e5bd3b8c24f061faa2dab1d
Rohit Kumar <un.rohitkumar@gmail.com>
2025-06-18 11:25:19 +0530

    DOM and BOM

---
+++ JavaScript/BOM_DOM/script.js
@@ -0,0 +1,7 @@
+console.log("window",window);
+console.log(windows.ducument);
+
+console.dir(document.children[0].children[1].children[0]);
+
+const heading = document.getElementsByTagName("h1");
+console.log("heading:", heading);
