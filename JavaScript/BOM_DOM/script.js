641c1b5ea3c8799376a3950d38250f911b0013e7
Rohit Kumar <un.rohitkumar@gmail.com>
2025-06-18 11:50:29 +0530

    query

--- JavaScript/BOM_DOM/script.js
+++ JavaScript/BOM_DOM/script.js
@@ -1,7 +1,21 @@
-console.log("window",window);
-console.log(windows.ducument);
+// console.log("window",window);
+// console.log(windows.ducument);
 
-console.dir(document.children[0].children[1].children[0]);
+// console.dir(document.children[0].children[1].children[0]);
 
-const heading = document.getElementsByTagName("h1");
-console.log("heading:", heading);
+// const heading = document.getElementsByTagName("h1");
+// console.log("heading:", heading);
+// const header = document.getElementsByTagName("header");
+// console.log("header:", header);
+
+// const heading = document.getElementsByTagName("h1");
+// if (heading.length > 0) {
+//   heading[0].innerText = "Hello Rohit!";
+// }
+
+const titles=document.querySelectorAll("h3");
+
+
+
+const textBrownElement=document;
+console.log("text para",textParas);
