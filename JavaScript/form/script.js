1000243addfff51cab47c533e65147e9f3afa850
Rohit Kumar <un.rohitkumar@gmail.com>
2025-06-20 09:16:31 +0530

    DOMDAYS

---
+++ JavaScript/form/script.js
@@ -0,0 +1,11 @@
+const handleUserName = () => {
+    console.log("typed");
+    console.log("--------");
+
+    // const elem= e.target}
+    // console.log(elem.value);
+    // title
+    
+    
+  
+};
